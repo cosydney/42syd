e01c8e908ce67a30ab1104bc47080728dc9b5d5b
cosydney <sycohen@e2r12p19.42.fr>
2016-09-01 09:42:19 +0200

    pointeurs

--- Day03C/tableaux.c
+++ Day03C/tableaux.c
@@ -4,16 +4,26 @@ void ft_put_addr(void *ptr);
 
 int main(void)
 {
-	int a; 
-	int b; 
-	int c;
+	int tab[10];
+	// pour acceder au dernier element tu tableau cest donc tab[ dernier element - 1] cad tab[9]
+	tab[0]=42;
+	// equivalent a *(tab + 0) = 42;
 
-	a = 23;
-	b = 42;
-	ptr = &b;
-	ft_putaddr(ptr + 1);
+	int tab2[10];
+	int *tabptr[2];
+
+
+	tabptr[0] = tab;
+	tabptr[1] = tab2;
+	tabptr[1][3]= 42;
+	// les deux | expressions sont equivalentes
+	*(*(tabptr + 1) + 3)= 42;
+
+
+
+	ft_putaddr(tab);
 	ft_putchar(' ');
-	ft_putnbr(*(ptr + 1));
+	ft_putnbr(tab[0];
 	ft_putchar('\n');
 	return (0);
 

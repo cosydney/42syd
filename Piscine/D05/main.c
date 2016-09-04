6d3089a4fdb67a3da839dfa63dac49e8a952a51b
cosydney <sycohen@e2r12p2.42.fr>
2016-09-04 19:07:57 +0200

    update

--- Piscine/D05/main.c
+++ Piscine/D05/main.c
@@ -6,7 +6,7 @@
 /*   By: sycohen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/04 13:21:28 by sycohen           #+#    #+#             */
-/*   Updated: 2016/09/04 16:18:54 by sycohen          ###   ########.fr       */
+/*   Updated: 2016/09/04 18:56:32 by sycohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,18 @@ int	main(void)
 	ft_putchar('\n');
 	ft_putchar('\n');
 
-	printf("#--Ex00: ft_putstr--#\n");
+	printf("#--Ex01: ft_putstr--#\n");
 	int b, a;
-	for( a = -5 ; a < 20 ; a++)
+	ft_putnbr(-2147483648);
+		ft_putchar('\n');
+	ft_putnbr(2147483647);
+		ft_putchar('\n');
+	for ( a = -100; a < 100; a++)
 	{
-		b = ft_putnbr(a);
-		printf("%d : %d\n", a, b);
+		ft_putnbr(a);
+		ft_putchar(' ');
 	}
+
+	printf("#--Ex02: ft_putstr--#\n");
 	return (0);
 }
---
+++ Piscine/rush00/colle00.c
@@ -0,0 +1,37 @@
+void    ft_putchar(char c);
+
+void    print_line(int width, char begin, char center, char end)
+{
+	int i;
+
+	i = 0;
+	if (width >= -1)
+		ft_putchar(begin);
+	while (i < width)
+	{
+		ft_putchar(center);
+		i++;
+	}
+	if (width > -1)
+		ft_putchar(end);
+	ft_putchar('\n');
+}
+
+void    colle(int x, int y)
+{
+	int i;
+
+	i = 2;
+	if (x > 0)
+	{
+		if (y > 0)
+			print_line(x - 2, 'o', '-', 'o');
+		while (i < y)
+		{
+			print_line(x - 2, '|', ' ', '|');
+			i++;
+		}
+		if (y > 1)
+			print_line(x - 2, 'o', '-', 'o');
+	}
+}
